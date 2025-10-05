db17082f919e359022c4519820449129174e6cc9
Tim Smart <hello@timsmart.co>
2025-10-05 22:06:07 +1300

    autoSetupRemote

--- home/tim/git.nix
+++ home/tim/git.nix
@@ -1,8 +1,17 @@
-{config, pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.git = {
     enable = true;
     userName = "Tim Smart";
     userEmail = "hello@timsmart.co";
+    extraConfig = {
+      push = {
+        autoSetupRemote = true;
+      };
+    };
   };
 
   home.packages = with pkgs; [gitAndTools.gh];
