39104090b6d6ee370892dc6ede1268bb69a9b132
Tim Smart <hello@timsmart.co>
2025-10-08 11:00:35 +1300

    add gap toggle

--- home/tim/aerospace.nix
+++ home/tim/aerospace.nix
@@ -8,7 +8,20 @@
     jankyborders
   ];
 
-  home.file.".aerospace.toml" = {
+  xdg.configFile."aerospace/aerospace.toml" = {
     source = config.lib.file.mkOutOfStoreSymlink "/Volumes/Code/dotfiles/home/tim/aerospace/aerospace.toml";
+  };
+  xdg.configFile."aerospace/gaps" = {
+    text = ''
+      #!/usr/bin/env zsh
+      if [[ -n "$1" ]]; then
+        size="[{ monitor.main = $1 }, 8]"
+      else
+        size="8"
+      fi
+      sed -i ''' 's/^outer\.left = .*/outer.left = '$size'/; s/^outer\.right = .*/outer.right = '$size'/' /Volumes/Code/dotfiles/home/tim/aerospace/aerospace.toml
+      aerospace reload-config
+    '';
+    executable = true;
   };
 }
