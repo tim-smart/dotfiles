31a5e821bc1f200b108e6be9662c3a344750a1ea
Tim Smart <hello@timsmart.co>
2026-03-26 15:07:14 +1300

    wip

--- home/tim/neovim/lua/config/lspconfig.lua
+++ home/tim/neovim/lua/config/lspconfig.lua
@@ -58,6 +58,12 @@ return function()
 
 	vim.lsp.config("oxlint", {
 		cmd = { "oxlint", "--lsp" },
+		root_dir = function(bufnr, on_dir)
+			local root_markers = { ".oxlintrc.json", "vite.config.ts" }
+			root_markers = vim.list_extend(root_markers, { ".git" })
+			local project_root = vim.fs.root(bufnr, root_markers)
+			on_dir(project_root or vim.fn.getcwd())
+		end,
 	})
 
 	vim.lsp.config("ts_ls", {
